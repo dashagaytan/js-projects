bf1a8e748db86b56e34ea4044749167f29087be3
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-12-07 15:10:06 -0600

    .

--- scrimba-js/main.js
+++ scrimba-js/main.js
@@ -1,6 +1,6 @@
 //create a function on click increments count by 1
 let count = 0
-let updateNum = document.getElementById("count-el")
+let updateNum = document.getElementById("count-el") //updates number on click by grabbing the id of the element
 
 function increment() {
     //alert("button was clicked")
@@ -13,5 +13,4 @@ function decrement() {
     //alert("button was clicked")
     count = count - 1
     updateNum.innerHTML = count
-
 }
