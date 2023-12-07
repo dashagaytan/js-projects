23e495fd81ca42e5d47a7d446f69731366555592
Dasha Gaytan <dasha.code.gaytan@gmail.com>
2023-12-07 15:33:34 -0600

    save results and display in DOM

--- scrimba-js/main.js
+++ scrimba-js/main.js
@@ -1,6 +1,7 @@
 //create a function on click increments count by 1
 let count = 0
 let updateNum = document.getElementById("count-el") //updates number on click by grabbing the id of the element
+let savedEl = document.getElementById("saved-el")  //p tag of all saved counts 
 
 function increment() {
     //alert("button was clicked")
@@ -9,8 +10,14 @@ function increment() {
 
 }
 
-function decrement() {
-    //alert("button was clicked")
-    count = count - 1
-    updateNum.innerHTML = count
+// function decrement() {
+//     //alert("button was clicked")
+//     count = count - 1
+//     updateNum.innerHTML = count
+// }
+
+function save(){
+    let savedCount = count + ", "
+    savedEl.innerHTML += savedCount
+    console.log(savedCount)
 }
